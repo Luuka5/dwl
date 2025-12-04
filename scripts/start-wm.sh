d38aa6b992fa41bed836dff331054940d275eb7c
Touko Päivänsalo <touko.paivansalo@gmail.com>
2025-12-05 00:56:07 +0200

    Simplify start-wm script

--- scripts/start-wm.sh
+++ scripts/start-wm.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
 
-dwl -s 'dwlb -hidden -font "JetBrains Mono NL:size=9:Symbols NerdFont:size=9"'
-#&> dwl.log
-#cp dwl.log dwl.log.bak
+dwl -s 'dwlb -font "monospace:size=9:Symbols NerdFont:size=9"'
 
