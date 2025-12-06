9dc80838759f659a939dfcc54d2bc63e6dfaff3f
Touko Päivänsalo <touko.paivansalo@gmail.com>
2025-12-06 12:12:50 +0200

    Remove killall

--- scripts/post-startup.sh
+++ scripts/post-startup.sh
@@ -9,13 +9,11 @@ dwlb -show HDMI-A-1
 
 run-status &
 
-killall kanshi 
 kanshi > /tmp/kanshi.log 2>&1 &
 
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 systemctl --user start graphical-session.target
 
-killall swayidle
 swayidle -w \
   timeout 1000 'lock' \
   timeout 1201 'systemctl suspend' \
